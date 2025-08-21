192b433fc2cdae89005f863e80e799636d38eece
Gabriel Garcia <gabrielproaj@gmail.com>
2025-08-20 21:49:28 -0300

    Domain model

---
+++ src/main/java/com/gamesgg/dslist/dto/GameDTO.java
@@ -0,0 +1,64 @@
+package com.gamesgg.dslist.dto;
+
+import com.gamesgg.dslist.entities.Game;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.BeanUtils;
+
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO(){
+    }
+
+    public GameDTO(Game entity){
+        BeanUtils.copyProperties(entity,this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+}
---
+++ src/main/java/com/gamesgg/dslist/entities/GameList.java
@@ -0,0 +1,53 @@
+package com.gamesgg.dslist.entities;
+
+import com.gamesgg.dslist.dto.GameMinDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList{
+
+    @Id
+    private Long id;
+    private String name;
+
+    public GameList(){
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(getId(), gameList.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
